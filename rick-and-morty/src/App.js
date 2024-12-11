af8e8c6fd962dcd10807e6fea31fa350db7febce
zeeshan4466 <zeeshan.zulfiqar11@gmail.com>
2024-12-11 14:44:25 +0500

    used Routes instead of Switch

--- rick-and-morty/src/App.js
+++ rick-and-morty/src/App.js
@@ -1,6 +1,6 @@
 // src/App.js
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Routes } from 'react-router-dom';
 import { Container, CssBaseline, AppBar, Toolbar, Typography, TextField } from '@mui/material';
 import Home from './Pages/Home';
 import CharacterDetail from './Pages/CharacterDetail';
@@ -10,40 +10,25 @@ function App() {
 
   return (
     <Router>
-      <div>
-        {/* App Bar */}
-        <AppBar position="sticky">
-          <Toolbar>
-            <Typography variant="h6">Rick and Morty Character Explorer</Typography>
-          </Toolbar>
-        </AppBar>
+    <div>
+      {/* App Bar */}
+      <AppBar position="sticky">
+        <Toolbar>
+          <Typography variant="h6">Rick and Morty Character Explorer</Typography>
+        </Toolbar>
+      </AppBar>
 
-        <CssBaseline />
-        
-        {/* Main Content */}
-        <Container>
-          <Switch>
-            <Route exact path="/">
-              {/* Home Page with search */}
-              <TextField
-                label="Search characters"
-                variant="outlined"
-                fullWidth
-                value={searchQuery}
-                onChange={(e) => setSearchQuery(e.target.value)}
-                sx={{ mb: 3, mt: 3 }}
-              />
-              <Home searchQuery={searchQuery} />
-            </Route>
-
-            <Route path="/character/:id">
-              {/* Character Detail Page */}
-              <CharacterDetail />
-            </Route>
-          </Switch>
-        </Container>
-      </div>
-    </Router>
+      <CssBaseline />
+      
+      {/* Main Content */}
+      <Container>
+        <Routes> {/* Replace Switch with Routes */}
+          <Route path="/" element={<Home searchQuery={searchQuery} />} /> {/* Home Page with search */}
+          <Route path="/character/:id" element={<CharacterDetail />} /> {/* Character Detail Page */}
+        </Routes>
+      </Container>
+    </div>
+  </Router>
   );
 }
 
